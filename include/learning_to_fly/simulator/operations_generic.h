b1d872ff7bd797088cb96c94f224b50b55598848
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-12-28 14:25:27 +0100

    thrust baseline

--- include/learning_to_fly/simulator/operations_generic.h
+++ include/learning_to_fly/simulator/operations_generic.h
@@ -232,6 +232,7 @@ namespace rl_tools{
             utils::assert_exit(device, env.current_dynamics.thrust_constants[1] == 0, "linear thrust coefficient not handled yet");
             T hover_thrust = env.current_dynamics.mass * (-1) * env.current_dynamics.gravity[2];
             env.parameters.mdp.reward.action_baseline = math::sqrt(device.math, (hover_thrust / 4 - env.current_dynamics.thrust_constants[0]) / env.current_dynamics.thrust_constants[2]);
+//            env.parameters.mdp.reward.action_baseline *= 0.8;
         }
 
     }
