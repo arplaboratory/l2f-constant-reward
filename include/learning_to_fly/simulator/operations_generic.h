35f04e8887b007fc964295e86b3bacd047ec3b44
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-01-10 19:51:52 +0400

    better init

--- include/learning_to_fly/simulator/operations_generic.h
+++ include/learning_to_fly/simulator/operations_generic.h
@@ -268,7 +268,8 @@ namespace rl_tools{
     static void initial_state(DEVICE& device, rl::environments::Multirotor<SPEC>& env, typename rl::environments::multirotor::StateRotors<T, TI, NEXT_COMPONENT>& state){
         initial_state(device, env, static_cast<NEXT_COMPONENT&>(state));
         for(typename DEVICE::index_t i = 0; i < 4; i++){
-            state.rpm[i] = (env.parameters.dynamics.action_limit.max - env.parameters.dynamics.action_limit.min) / 2 + env.parameters.dynamics.action_limit.min;
+//            state.rpm[i] = (env.parameters.dynamics.action_limit.max - env.parameters.dynamics.action_limit.min) / 2 + env.parameters.dynamics.action_limit.min;
+            state.rpm[i] = env.parameters.dynamics.hovering_throttle;
         }
     }
     template<typename DEVICE, typename T, typename TI_H, TI_H HISTORY_LENGTH, typename SPEC, typename NEXT_COMPONENT>
