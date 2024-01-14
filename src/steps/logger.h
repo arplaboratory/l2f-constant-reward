a3d8389d9d60c8b6fc6c95c00925ef0ce7afdf2b
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-01-14 21:54:22 +0400

    Reducing log load significantly

--- src/steps/logger.h
+++ src/steps/logger.h
@@ -3,6 +3,12 @@ namespace learning_to_fly {
         template <typename T_CONFIG>
         void logger(TrainingState<T_CONFIG>& ts){
             rlt::set_step(ts.device, ts.device.logger, ts.step);
+//            if(ts.step % 100000 == 0){
+//                std::cout << "Logged topics:" << std::endl;
+//                for (const auto &[key, value] : ts.device.logger.topic_frequency_dict) {
+//                    std::cout << "Key: " << key << ", Value: " << value << std::endl;
+//                }
+//            }
         }
     }
 }
