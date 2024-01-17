2519becfaab6416da425d375e047e2b23db3dc02
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-01-17 19:24:25 +0400

    backporting dr to validation

--- src/validation.cpp
+++ src/validation.cpp
@@ -26,6 +26,7 @@ struct ABLATION_SPEC: learning_to_fly::config::DEFAULT_ABLATION_SPEC{
     static constexpr bool EXPLORATION_NOISE_DECAY = false;
     static constexpr bool DISTURBANCE = false;
     static constexpr bool OBSERVATION_NOISE = false;
+    static constexpr bool DOMAIN_RANDOMIZATION = false;
 };
 
 
