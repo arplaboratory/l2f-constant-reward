9edcf8b80a52c5ebe1e9062fc49b42f3d64a4e8b
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-05-01 20:54:38 -0400

    this yielded a good policy

--- src/training.cpp
+++ src/training.cpp
@@ -52,7 +52,7 @@ struct ABLATION_SPEC: learning_to_fly::config::DEFAULT_ABLATION_SPEC{
     static constexpr bool ENABLE_CURRICULUM = false;
     static constexpr bool USE_INITIAL_REWARD_FUNCTION = true;
     static constexpr bool EXPLORATION_NOISE_DECAY = false;
-    static constexpr bool DISTURBANCE = false;
+    static constexpr bool DISTURBANCE = true;
     static constexpr bool OBSERVATION_NOISE = false;
     static constexpr bool DOMAIN_RANDOMIZATION = false;
 };
